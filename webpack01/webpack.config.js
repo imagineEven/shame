b4069084aff25392c4c20c3af5369c89376234c8
imagineEven <1016000928@qq.com>
2019-02-26 18:27:01 +0800

    recommend webpack01

--- webpack01/webpack.config.js
+++ webpack01/webpack.config.js
@@ -1,8 +1,12 @@
 let path = require('path');
 module.exports = {
+  // 入口文件
   entry: './src/script/main.js',
+  // 出口文件
   output: {
+    // 打包之后的文件名字
     filename: 'bunld.js',
+    // 打包的路径
     path: path.resolve('./dist/js')
   }
 }
