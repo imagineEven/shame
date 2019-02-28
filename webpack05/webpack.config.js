3d6dbd28aa1670ad6951fc5b5431329fe4c4dddc
imagineEven <1016000928@qq.com>
2019-02-28 18:10:15 +0800

    postcss-loader has bug

--- webpack05/webpack.config.js
+++ webpack05/webpack.config.js
@@ -4,7 +4,7 @@ let htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: 'js/[name] + [chunkhash].js',
+    filename: 'js/[name]-[chunkhash].js',
     path: path.resolve('./dist')
   },
   module: {
@@ -17,9 +17,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        exclude: __dirname + 'node_modules', 
         // css-loader 处理css文件
         // style-loader 创建style标签插入index.html
-        loader: 'style-loader!css-loader'
+        // postcss-loader 给类似flex等样式加前缀；
+        use: ['style-loader', 'css-loader', 'postcss-loader']
       }
     ]
   },
