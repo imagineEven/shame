63bfe9c083d3f0505755edc8135514ae9f7fc23b
imagineEven <1016000928@qq.com>
2019-03-01 13:01:24 +0800

    add-mini css extract plugin

--- webpack05/webpack.config.js
+++ webpack05/webpack.config.js
@@ -1,27 +1,31 @@
 let path = require('path');
 let htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: 'js/[name]-[chunkhash].js',
+    filename: 'js/[name].js',
     path: path.resolve('./dist')
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: __dirname + 'node_modules', 
+        exclude: __dirname + 'node_modules',
         include: __dirname + 'src',
         loader: 'babel-loader'
       },
       {
         test: /\.css$/,
-        exclude: __dirname + 'node_modules', 
+        exclude: __dirname + 'node_modules',
         // css-loader 处理css文件
         // style-loader 创建style标签插入index.html
         // postcss-loader 给类似flex等样式加前缀；
-        use: ['style-loader', 'css-loader', 'postcss-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader', 'postcss-loader'
+        ]
       }
     ]
   },
@@ -30,6 +34,11 @@ module.exports = {
       filename: 'index.html',
       template: 'index.html',
       inject: 'body'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "css/[name].css"
     })
   ]
 }
