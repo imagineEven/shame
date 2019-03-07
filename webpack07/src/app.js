619d68cb0a1091700273f5fc4f964a7d9bda4acb
imagineEven <1016000928@qq.com>
2019-03-07 15:37:22 +0800

    正则表达式

--- webpack07/src/app.js
+++ webpack07/src/app.js
@@ -3,6 +3,14 @@ import dom from './components/component.html';
 let nihao = 'it is remind you of sth'
 function app() {
   console.log(nihao)
+  let necessary = 'but it <br> dith 23 </br> of 22 with';
+  // 这样的话只会替换一次： 返回新的字符串： 没有改变原有字符串；
+  // let section = necessary.replace(/\<\/*br\>/g, '/')
+
+  // 返回的是匹配到后的数组；
+  let section = necessary.match(/\<\/*br\>/g);
+  console.log('匹配后的字符串',section);
+  console.log('原有字符串',necessary);
 }
 app();
 let appDom = document.querySelector('#app');
