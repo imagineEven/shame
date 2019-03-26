5c2d1a80ab283efcf7deb22754d2093b53d2ed4e
imagineEven <1016000928@qq.com>
2019-03-26 19:11:18 +0800

    es6-export export default

--- shame01/src/components/es6Module/es6Module.js
+++ shame01/src/components/es6Module/es6Module.js
@@ -1,10 +1,15 @@
 console.log('----------------下面是es6模块化');
+import height from './otherModule';
+// import
+// 引入的变量都是可读的，不允许改写接口，可以改写接口变量；
+// example: var height = 23; 报错；
+// 暴漏接口的时候 他会隐藏的做一些操作，让接口可读不可以改；
 
 
-import {age, height}  from './otherModule';
 // variable += 1
 // age += 1
 // let age = age + 1;
-console.log('age', age)
+console.log('height', height)
 // import { nihao } from './otherModule'
 // console.log('variable', nihao)
+
--- shame01/src/components/es6Module/otherModule.js
+++ shame01/src/components/es6Module/otherModule.js
@@ -1,5 +1,20 @@
 //导出多个数据,单个数据也一样。
-export {age,height};
+
 //方式二(推荐)可以一起在模块结束位置输出
 let age = 0;
 let height = 14;
+
+
+// export
+// export命令规定的是对外的接口,必须与模块内部的变量建立一一对应关系。动态绑定关系； 指向的是这个对象地址；不是copy;
+// 而commen.js 模块化暴漏的是拷贝后的变量;
+// 动态绑定关系；
+// 先执行关键字；让关键字指向undefined;
+// 在 import的时候可以使用 as 关键字；
+
+// export default
+// export default就是输出一个叫做default的变量或方法;
+// 不可以使用as 关键字； 
+export {
+  height
+}
