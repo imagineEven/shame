695d65293182ce9570d88f5591785beefe83efc9
imagineEven <1016000928@qq.com>
2019-04-19 17:10:50 +0800

    study-async-await

--- shame01/src/components/es6Module/es6Module.js
+++ shame01/src/components/es6Module/es6Module.js
@@ -17,6 +17,38 @@ class Es6Module extends BaseLogic {
   constructor() {
     super()
     console.log('----------------下面是es6模块化');
+    // this.gate();
+    this.createUI();
+  }
+
+  createUI() {
+    let lunch = this.createLunch()
+    console.log('lunch', lunch);
+  }
+
+  createLunch() {
+    new Promise(resolve => {
+      console.log('我这里面有执行吗')
+      // 这个return是Promise回调函数的，并不能代表最外层的函数；
+      // 在回调函数里 return 是没有什么意思的，
+      return 111111
+    })
+  }
+
+  async gate() {
+    // 等待Promise;
+    let nihao = await this.navigate();
+    console.log(nihao);
+    console.log('我是什么时候执行的？');
+  }
+
+  navigate() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('this is the navigation');
+        resolve(11111);
+      }, 2000);
+    })
   }
 }
 
