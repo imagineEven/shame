eb2ce8b1ed1ecab2d07bc7b7932bcc352c4b9a6d
imagineEven <1016000928@qq.com>
2019-05-16 12:09:21 +0800

    react

--- react02/src/compotents/Even/Even.js
+++ react02/src/compotents/Even/Even.js
@@ -13,33 +13,55 @@ class EvenTwo extends React.Component {
     super(props);
     // 定义状态；
     this.state = {
-      list: ['study English', 'study React']
+      list: ['study English', 'study React'],
+      inputValue: ''
     };
-    // this.createUI();
   }
 
   handleClickButton() {
-    console.log('我被点击了');
-    console.log('arr', this.arr);
-    // this.arr.push('abc');
+    // 出发update流程；改变视图
+    // 有可能是异步的 所以参与计算的时候 尽量使用函数
+    this.setState({
+      'list': [...this.state.list, this.state.inputValue]
+    });
+    this.setState({
+      inputValue: ''
+    });
+    // 这样写的话 防止异步带来的干扰；
+    // this.setState((prevState, props) => {
+    //   return { 'list': [...prevState.list, 'hello world'] };
+    // });
+
+  }
+
+  clickChangeInput(e) {
+    this.setState({
+      'inputValue': e.target.value
+    });
+  }
+
+  deletOption(index) {
+    let newArr = [...this.state.list];
+    newArr.splice(index, 1);
+    this.setState({
+      'list': newArr
+    });
   }
 
   // 提供组件显示的内容；提供需要渲染的dom;
   render() {
+    // 不会同步页面；why? 这里的状态不会做任何的监听
     this.arr = [123, 123, 123];
     return (
       // jsx 里面只能写表达式，不能写语句列如 {if(true) {return 234}}
       // 但是语句里面可以写标签；
       <div>
-        <input/>
-        <button onClick={this.handleClickButton}>add</button>
+        <input value={this.state.inputValue} onChange={this.clickChangeInput.bind(this)}/>
+        <button onClick={this.handleClickButton.bind(this)}>add</button>
         <ul>
-          {console.log('arr', this.arr)}
-          {console.log('list', this.state.list)}
-          {[
-            <li>asdf</li>,
-            <li>asdfasdf</li>
-          ]}
+          {this.state.list.map((item, index) => {
+            return <li key={index} onClick={this.deletOption.bind(this, index)}>{item}</li>;
+          })}
         </ul>
       </div>
     );
