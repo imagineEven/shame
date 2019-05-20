01109927753e8dd03e93e2ac48b5c4f2e8037788
imagineEven <1016000928@qq.com>
2019-05-20 14:09:43 +0800

    hase-bug

--- react02/src/App.js
+++ react02/src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import './App.css';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+// import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
+import { EvenTwo } from './compotents/Even/Even';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div className="list">
-          <Link to="/todoList">我要去todoList页面</Link>
-          <Link to="/other">我要去其他页面</Link>
-        </div>
-        <div className="RouteWrapper">
-          <Route to="/todoList"></Route>
-          <Route to="/other"></Route>
-        </div>
-      </Router>
-    </div>
-  );
+// 一切皆是组件；class EvenTwo extends React.Component {
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HashRouter>
+          <Switch>
+            <div className="list">
+              <Link to="/eventwo">我要去todoList页面</Link>
+              <Link to="/other">我要去其他页面</Link>
+              <div className="routeWrapper">
+                <Route path="/eventwo" component={EvenTwo}></Route>
+                <Route path="/other" component={Other}></Route>
+              </div>
+            </div>
+          </Switch>
+        </HashRouter>
+      </div>
+    );
+  }
+}
+
+class Other extends React.Component {
+  render() {
+    return (
+      <div>我是other组件</div>
+    );
+  }
 }
 
 export default App;
--- react02/src/index.js
+++ react02/src/index.js
@@ -5,14 +5,14 @@ import './index.css';
 
 // 组件app 比喻以大写字母开头
 import App from './App';
-import { EvenOne, EvenTwo } from './compotents/Even/Even';
+// import { EvenOne, EvenTwo } from './compotents/Even/Even';
 
 import * as serviceWorker from './serviceWorker';
 
 // 将虚拟dom 插入节点；
 // <EvenTwo /> 为jsx标签；自定义便签名字；
 // jsx作用, 将类转换成虚拟dom;
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
