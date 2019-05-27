46cc3367fd4c9de89e88b20c4f623bc027bce0ea
imagineEven <1016000928@qq.com>
2019-05-27 12:31:05 +0800

    init-reset-border-css

--- vue01/src/App.vue
+++ vue01/src/App.vue
@@ -6,17 +6,13 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
-<style>
+<style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 22px;
+  box-sizing: border-box;
 }
 </style>
--- vue01/src/components/HelloWorld.vue
+++ vue01/src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="helloWrapper">
     <div class="content">I am Even</div>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
   </div>
 </template>
 
